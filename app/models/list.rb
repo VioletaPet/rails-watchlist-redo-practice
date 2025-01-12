4a2300df9fe6f5dd670f8fe8be367a683aba134a
Violeta Petkovic <v.petkovic@protonmail.com>
2025-01-12 14:00:22 +0100

    creates associations and validations of models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+
+  validates :name, presence: { message: "Name can't be blank" }, uniqueness: { message: "Name must be unique"}
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+  validates :title, presence: { message: "Title can't be blank" }, uniqueness: { message: "Title must be unique" }
+  validates :overview, presence: { message: "Overview can't be blank" }
 end
